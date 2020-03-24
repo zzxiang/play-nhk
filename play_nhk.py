12e2036458e53f4fcb84a12b506524d267dee425
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2020-03-24 09:55:11 +0900

    Revert "Play the audio with web browser"
    This reverts commit c91a5af74fc423da33cc35564985efc05abac832.
    
    The web browser way doesn't work when the screen is locked.

--- play_nhk.py
+++ play_nhk.py
@@ -2,9 +2,30 @@
 
 import datetime
 import feedparser
-import webbrowser
+import vlc
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QLabel
 
 data = feedparser.parse('https://www.nhk.or.jp/r-news/podcast/nhkradionews.xml')
 url = data['entries'][0]['links'][0]['href']
 print(datetime.datetime.now(), url)
-webbrowser.open(url)
+player = vlc.MediaPlayer(url)
+player.play()
+
+app = QApplication(sys.argv)
+widget = QWidget()
+widget.resize(250, 150)
+widget.move(300, 300)
+widget.setWindowTitle('NHKラジオ')
+
+layout = QHBoxLayout(widget)
+widget.setLayout(layout)
+
+label = QLabel('NHKランジオ')
+label.setAlignment(Qt.AlignCenter)
+layout.addWidget(label)
+
+widget.show()
+
+sys.exit(app.exec_())
